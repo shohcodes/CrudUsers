8e35608c1f1b9c3223b0b29f3bfde23476ff5ec4
shohcodes <zokirislom01@gmail.com>
2023-09-27 02:20:14 +0500

    database settings

--- config/database.py
+++ config/database.py
@@ -12,7 +12,7 @@ HOST = str(os.getenv('HOST'))
 DB_NAME = os.getenv('DB_NAME')
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:5432/{DB_NAME}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
